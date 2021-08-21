9f865cd502416a1eb9dcdd41bfb556b449291889
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-21 15:32:51 -0300

    Initial commit

---
+++ src/main/java/EleicaoDeLider.java
@@ -0,0 +1,53 @@
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import static org.apache.zookeeper.Watcher.Event.EventType.None;
+import java.io.IOException;
+
+public class EleicaoDeLider {
+
+    private static final String HOST = "localhost";
+    private static final String PORTA = "2181";
+    private static final int TIMEOUT = 5000;
+    private ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Método main: " + Thread.currentThread().getName());
+        EleicaoDeLider eleicaoDeLider = new EleicaoDeLider();
+        eleicaoDeLider.conectar();
+        eleicaoDeLider.executar();
+        eleicaoDeLider.fechar();
+    }
+
+    public void fechar () throws InterruptedException{
+        zooKeeper.close();
+    }
+
+    public void executar () throws InterruptedException{
+        synchronized (zooKeeper){
+            zooKeeper.wait();
+        }
+    }
+
+    public void conectar () throws IOException {
+        zooKeeper = new ZooKeeper(
+                String.format("%s:%s", HOST, PORTA),
+                TIMEOUT,
+                evento -> {
+                    if (evento.getType() == None){
+                        if (evento.getState() == Watcher.Event.KeeperState.SyncConnected){
+                            System.out.println("Tratando evento: " + Thread.currentThread().getName());
+                            System.out.println ("Conectou");
+                        }
+                        else if (evento.getState() == Watcher.Event.KeeperState.Disconnected){
+                            synchronized (zooKeeper){
+                                System.out.println ("Desconectou");
+                                System.out.println ("Estamos na thread: " + Thread.currentThread().getName());
+                                zooKeeper.notify();
+                            }
+                        }
+                    }
+                }
+        );
+    }
+}
