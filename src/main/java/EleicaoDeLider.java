855e2198e4db0b08b1c734f94cf8c492fdb5f934
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-22 20:40:19 -0300

    Eleicao de lider funcionando. Programa pode ser executado por multiplos processos. Cada um faz a sua candidatura. Aquele que obtiver a de menor numero se torna o lider. Todos ficam sabendo quem é o lider

--- src/main/java/EleicaoDeLider.java
+++ src/main/java/EleicaoDeLider.java
@@ -1,47 +1,61 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
 import static org.apache.zookeeper.Watcher.Event.EventType.None;
+
+import org.apache.zookeeper.*;
+
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class EleicaoDeLider {
-
     private static final String HOST = "localhost";
     private static final String PORTA = "2181";
     private static final int TIMEOUT = 5000;
+    private static final String NAMESPACE_ELEICAO = "/eleicao";
+    //variável que armazena o nome do ZNodeAtual
+    // cada processo que executa uma instância deste programa tem o seu
+    private String nomeDoZNodeDesseProcesso;
     private ZooKeeper zooKeeper;
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("Método main: " + Thread.currentThread().getName());
-        EleicaoDeLider eleicaoDeLider = new EleicaoDeLider();
-        eleicaoDeLider.conectar();
-        eleicaoDeLider.executar();
-        eleicaoDeLider.fechar();
-    }
 
     public void fechar () throws InterruptedException{
         zooKeeper.close();
     }
 
-    public void executar () throws InterruptedException{
-        synchronized (zooKeeper){
-            zooKeeper.wait();
-        }
+    public void realizarCandidatura () throws InterruptedException, KeeperException {
+        //o prefixo
+        String prefixo = String.format("%s/cand_", NAMESPACE_ELEICAO);
+        //parâmetros: prefixo, dados a serem armazenados no ZNode, lista de segurança e tipo do ZNode
+        String pathInteiro = zooKeeper.create(prefixo, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        //exibe o path inteiro
+        System.out.println(pathInteiro);
+        //armazena somente o nome do ZNode recém criado
+        this.nomeDoZNodeDesseProcesso = pathInteiro.replace(String.format("%s/", NAMESPACE_ELEICAO), "");
     }
 
-    public void conectar () throws IOException {
+    public void elegerOLider () throws InterruptedException, KeeperException {
+        //o segundo parâmetro (watch) indica que não queremos registrar um manipulador de eventos
+        List <String> candidatos = zooKeeper.getChildren(NAMESPACE_ELEICAO, false);
+        //ordena in-place: a coleção é alterada
+        Collections.sort(candidatos);
+        String oMenor = candidatos.get(0);
+        if (oMenor.equals(nomeDoZNodeDesseProcesso))
+            System.out.printf("Me chamo %s e sou o líder", nomeDoZNodeDesseProcesso);
+        else
+            System.out.printf("Me chamo %s e não sou o líder. O líder é o %s.", nomeDoZNodeDesseProcesso, oMenor);
+    }
+
+    public void conectar () throws IOException{
         zooKeeper = new ZooKeeper(
                 String.format("%s:%s", HOST, PORTA),
                 TIMEOUT,
-                evento -> {
-                    if (evento.getType() == None){
-                        if (evento.getState() == Watcher.Event.KeeperState.SyncConnected){
-                            System.out.println("Tratando evento: " + Thread.currentThread().getName());
-                            System.out.println ("Conectou");
+                event -> {
+                    if (event.getType() == None){
+                        if (event.getState() == Watcher.Event.KeeperState.SyncConnected){
+                            System.out.println ("Conectou!!");
+                            System.out.printf("Estamos na thread: %s\n", Thread.currentThread().getName());
                         }
-                        else if (evento.getState() == Watcher.Event.KeeperState.Disconnected){
+                        else if (event.getState() == Watcher.Event.KeeperState.Disconnected){
                             synchronized (zooKeeper){
-                                System.out.println ("Desconectou");
+                                System.out.println ("Desconectou...");
                                 System.out.println ("Estamos na thread: " + Thread.currentThread().getName());
                                 zooKeeper.notify();
                             }
@@ -49,5 +63,19 @@ public class EleicaoDeLider {
                     }
                 }
         );
+    }
+    public void executar() throws  InterruptedException{
+        synchronized (zooKeeper){
+            zooKeeper.wait();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        EleicaoDeLider eleicaoDeLider = new EleicaoDeLider();
+        eleicaoDeLider.conectar();
+        eleicaoDeLider.realizarCandidatura();
+        eleicaoDeLider.elegerOLider();
+        eleicaoDeLider.executar();
+        eleicaoDeLider.fechar();
     }
 }
